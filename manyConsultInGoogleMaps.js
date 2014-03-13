227af154ee9f942dc1138007f92badb912cd57be
Fabian <fabideleon01@gmail.com>
2014-03-13 14:38:25 -0700

    Update manyConsultInGoogleMaps.js

---
+++ manyConsultInGoogleMaps.js
@@ -0,0 +1,23 @@
+/**call many times api of google maps*/
+
+setInterval(function(){
+   $.ajax({
+					 url: 'http://maps.googleapis.com/maps/api/geocode/json?address=' + clientAdress + 'city=' + clientCity + 'zipcode=' + clientZipcode + '&sensor=false',
+					 type: "POST",
+					 data: {
+						 'x': 'x' 
+					 },
+					 dataType: "html",
+					 success: function(msg) {
+						try{
+						 msg=JSON.parse(msg);											
+						var adress = msg.results[0].formatted_address;	
+						adress = adress.split(",")[0];
+						adress = adress.replace(/ /g,"");
+						var myLatitud = msg.results[0].geometry.location.lat;
+						var myLongitud = msg.results[0].geometry.location.lng; 
+						//var myCity = msg.results[0].address_components[1].long_name;
+						}catch(e){} 
+					}
+		  });
+),2000};
